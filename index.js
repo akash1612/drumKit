a8c4eabbf9e94d254ab5f97f31f1d6a286026ec3
Aditya <adityaakash1612@gmail.com>
2020-07-16 16:47:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+for (var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+            audioText=this.innerHTML;
+            playSound(audioText);
+            this.classList.add("pressed");
+            setTimeout(()=>{
+                    this.classList.remove("pressed")
+            },100);
+});
+}
+
+document.addEventListener("keydown",(event)=>{
+        keypressed=event.key;
+        playSound(keypressed);
+        document.querySelector("."+keypressed).classList.add("pressed");
+        setTimeout(()=>{
+                document.querySelector("."+keypressed).classList.remove("pressed"); 
+        },100);
+});
+
+function playSound(text){
+        switch (text) {
+                case "w":
+                        var crash=new Audio("sounds/crash.mp3");
+                        crash.play();
+                        break;
+                case "a":
+                        var kick=new Audio("sounds/kick-bass.mp3");
+                        kick.play();
+                        break;
+                case "s":
+                        var snare=new Audio("sounds/snare.mp3");
+                        snare.play();
+                        break;        
+                case "d":
+                        var tom1=new Audio("sounds/tom-1.mp3");
+                        tom1.play();
+                        break;
+                case "j":
+                        var tom2=new Audio("sounds/tom-2.mp3");
+                        tom2.play();
+                        break;            
+                case "k":
+                        var tom3=new Audio("sounds/tom-3.mp3");
+                        tom3.play();
+                        break;
+                case "l":
+                        var tom4=new Audio("sounds/tom-4.mp3");
+                        tom4.play();
+                        break;
+                default:
+                        break;
+        }
+}
